df41a8f418a0395720bef2438f02bce6c5a2523b
Danilyns <sanchezdanilyn@gmail.com>
2024-02-16 03:25:05 -0600

    Finally figured out why my database had duplicate names and it was because of the Faker within the find_or_create_by() methods. To solve this I just left the fakers at the very end before the loop ends using the update() method.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source "https://rubygems.org"
 
 ruby "3.1.2"
 
+#add gem
+gem 'faker'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,13 @@
 require 'csv'
 
-# delete all data from tables to prevent it from duplicating rows everytime you do db:seeds
-# Delete records in reverse order to avoid foreign key constraint violations
+# Delete all data from tables to prevent it from duplicating rows every time you do db:seeds
 Hotel.delete_all
-Attraction.delete_all
 City.delete_all
 Country.delete_all
 
-
-#fetch the filename
+#################################################################
+# Fetch the Hotels filename
+#################################################################
 filename = Rails.root.join("db/hotels.csv")
 
 puts "Loading Hotels from #{filename}"
@@ -16,31 +15,72 @@ puts "Loading Hotels from #{filename}"
 csv_data = File.read(filename)
 hotels = CSV.parse(csv_data, headers: true, encoding: "utf-8")
 
-puts "Parsed Hotels:"
-puts hotels.inspect
-
 hotels.each do |h|
-  # Create or find country record
-  country = Country.find_or_create_by(name: h["countyName"])
+  # Lorem paragraph
+  description = Faker::Lorem.paragraph
 
-  # Create or find city record
-  city = City.find_or_create_by(
-    name: h["cityName"],
-    country: country
-    )
-
-  # Create attraction record
-  Attraction.find_or_create_by(
-    city: city,
-    description: h["Attractions"]
+  # Find or create the country record
+  country = Country.find_or_create_by(
+    name: h["countyName"],
+    country_code: h["countyCode"]
   )
 
-  # Create hotel record
-  Hotel.find_or_create_by(
+  # Find or create the city record
+  city = City.find_or_create_by(
+    name: h["cityName"],
+    airport_code: h["airport_code"],
+    country: country
+  )
+
+  # Create the hotel record
+  Hotel.create(
     name: h["HotelName"],
-    rating: h["HotelRating"],
-    address: h["Address"],
     description: h["Description"],
+    address: h["Address"],
+    rating: h["HotelRating"],
+    url: h["HotelWebsiteUrl"],
+    city: city
+  )
+
+  # Use Lorem WITHOUT creating duplicates of the same name! This must be done outside the find_or_create_by method
+  country.update(description: Faker::Lorem.paragraph)
+  city.update(description: Faker::Lorem.paragraph)
+
+end
+
+puts "Created #{City.count} cities..."
+puts "Created #{Country.count} countries"
+puts "Created #{Hotel.count} hotels..."
+
+#################################################################
+# Fetch the Attractions filename
+#################################################################
+attractions_filename = Rails.root.join("db/attractions.csv")
+
+puts "Loading Attractions from #{attractions_filename}"
+
+csv_data = File.read(attractions_filename)
+attractions = CSV.parse(csv_data, headers: true, encoding: "ISO-8859-1") #use ISO-8859-1 and NOT utf-8 bc ISO uses a wide variety of letters which is need for the img URLs
+
+attractions.each do |a|
+  # Find the city
+  city = City.find_by(name: a["City"])
+
+  # Count of cities not found
+  cities_not_found = 0
+  if !city
+    cities_not_found += 1
+    next
+  end
+
+  # Create the attraction record
+  Attraction.create(
+    name: a["Name"],
+    image: a["Image"],
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraph,
     city: city
   )
 end
+
+puts "Created #{Attraction.count} attractions"
