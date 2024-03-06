c66b42235db14a009c6564eebd2d2bb348393ca2
Danilyns <sanchezdanilyn@gmail.com>
2024-03-05 22:41:40 -0600

    Added simple search for Hotel names, located in the header partial

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,7 +1,15 @@
 class HotelsController < ApplicationController
   def index
     #@hotels = Hotel.all
-    @hotels = Hotel.page(params[:page]).per(50) #add new page per 50 rows
+
+    if params[:search].present?
+      # Search and paginate the results
+      @hotels = Hotel.where('name LIKE ?', "%#{params[:search]}%")
+                     .page(params[:page]).per(50)
+    else
+      # Just paginate without search
+      @hotels = Hotel.page(params[:page]).per(50)
+    end
   end
 
   def show
