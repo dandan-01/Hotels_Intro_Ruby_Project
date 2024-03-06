db21f8a0974c7740a066332032a7a3fab9ef063f
Danilyns <sanchezdanilyn@gmail.com>
2024-03-06 00:29:17 -0600

    Finished the Home page to include data sets from both Hotels and Attractions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @hotels = Hotel.all
+    #show hotel and attraction info in home page
+    @hotels = Hotel.limit(200) #limit to 200 so it loads up more quickly
+    @attractions = Attraction.all
   end
 end
